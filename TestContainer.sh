6c19ad08e38a54a85b1489a54e33e43cbf4ebbff
Ben Newman <BenNewman@outlook.com>
2018-08-13 23:35:15 +0100

    Run dependabot tests

--- TestContainer.sh
+++ TestContainer.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# Restore ruby deps
 cd /src/dependabot-core
-
 bundle install --path vendor/bundle
+
+# Restore deps for each package manager
 cd helpers/yarn && yarn install && cd /src/dependabot-core
 cd helpers/npm && yarn install && cd /src/dependabot-core
 cd helpers/gradle/buildfile_parser && gradle jar && mv build/libs/buildfile_parser.jar ../ && cd /src/dependabot-core
@@ -10,3 +12,8 @@ cd helpers/gradle/settings_file_parser && gradle jar && mv build/libs/settings_f
 cd helpers/php && composer install && cd /src/dependabot-core
 cd helpers/python && pyenv exec pip install -r requirements.txt && pyenv local 2.7.15 && pyenv exec pip install -r requirements.txt && pyenv local --unset && cd /src/dependabot-core
 cd helpers/elixir && mix deps.get && cd /src/dependabot-core
+
+# Run dependabot tests
+cd helpers/yarn && node_modules/.bin/jest && cd /src/dependabot-core
+cd helpers/npm && node_modules/.bin/jest && cd /src/dependabot-core
+bundle exec rspec spec
